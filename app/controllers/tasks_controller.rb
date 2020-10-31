f72071cfd9890abe8bee0497252c2ec5c7c3b61a
Sandy <sandyvasquez9@gmail.com>
2020-10-30 23:09:56 -0700

    wave 2 mostly done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,32 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(book_params)
+
+    if @task.save
+      redirect_to :action => 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+    Task.find(params[:id]).destroy
+    redirect_to :action => 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
+  get 'tasks/show'
+  get 'tasks/new'
+  post '/tasks', to: 'tasks#new'
 
+  # routes that operate on individual task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#new'
+
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
